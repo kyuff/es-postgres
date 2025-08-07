d5e9707f4ce1027bb3ab6efb5402c5125f2fe304
kyuff <soren@sxl.dk>
2025-08-07 16:06:33 +0200

    Finishes the Processor

--- internal/processor/types.go
+++ internal/processor/types.go
@@ -3,9 +3,11 @@ package processor
 import (
 	"context"
 	"iter"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/kyuff/es"
+	"github.com/kyuff/es-postgres/internal/database"
 )
 
 type Connector interface {
@@ -14,4 +16,9 @@ type Connector interface {
 
 type Reader interface {
 	Read(ctx context.Context, streamType, streamID string, eventNumber int64) iter.Seq2[es.Event, error]
+}
+
+type Schema interface {
+	SelectOutboxWatermark(ctx context.Context, db database.DBTX, stream database.Stream) (database.OutboxWatermark, int64, error)
+	UpdateOutboxWatermark(ctx context.Context, db database.DBTX, stream database.Stream, delay time.Duration, watermark database.OutboxWatermark) error
 }
