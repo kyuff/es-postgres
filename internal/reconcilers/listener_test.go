fce743712940ab95352df3e59d415b34762919d7
kyuff <soren@sxl.dk>
2025-08-21 20:17:17 +0200

    Adds initial listener_test

---
+++ internal/reconcilers/listener_test.go
@@ -0,0 +1,31 @@
+package reconcilers_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kyuff/es-postgres/internal/assert"
+	"github.com/kyuff/es-postgres/internal/database"
+	"github.com/kyuff/es-postgres/internal/reconcilers"
+)
+
+func TestNewListener(t *testing.T) {
+	t.Run("should listen to the outbox", func(t *testing.T) {
+		// arrange
+		var (
+			listener = reconcilers.NewListener()
+
+			p = &ProcessorMock{}
+		)
+
+		p.ProcessFunc = func(ctx context.Context, stream database.Stream) error {
+			return nil
+		}
+
+		// act
+		err := listener.Reconcile(t.Context(), p)
+
+		// assert
+		assert.NoError(t, err)
+	})
+}
